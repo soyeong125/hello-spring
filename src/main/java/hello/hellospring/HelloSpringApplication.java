b2dee9c6ad1fd5cdc85a0bbb63d4eefeb3628e43
choisoyeong <thdud3410@gmail.com>
2023-04-12 15:49:23 +0900

    배포 자동화 구현

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -2,12 +2,16 @@ package hello.hellospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloSpringApplication.class, args);
+		//SpringApplication.run(HelloSpringApplication.class, args);
+		SpringApplication application = new SpringApplication(HelloSpringApplication.class);
+		application.addListeners(new ApplicationPidFileWriter());
+		application.run(args);
 	}
 
 }
