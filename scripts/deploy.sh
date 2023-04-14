96b5cfc6092c5c7cc8643a0e9275e5b2801d2399
choisoyeong <thdud3410@gmail.com>
2023-04-14 21:34:08 +0900

    jar 실행파일 위치 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@ REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=hello-spring
 
 echo "> Build 파일 복사"
-cp $REPOSITORY/zip/deploy/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 CURRENT_PID=$(pgrep -f $PROJECT_NAME)
